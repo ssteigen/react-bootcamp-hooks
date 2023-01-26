e91db7e7f8b48fc3d0cf4906be7a33c25392fb4a
Steigen, Savanah <steigen@theinstitutes.org>
2023-01-26 10:46:51 -0500

    Refector useLocalStorageState into useLocalStorageReducer

--- src/contexts/todos.context.js
+++ src/contexts/todos.context.js
@@ -1,22 +1,27 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext } from "react";
 import todoReducer from "../reducers.js/todo.reducer";
+import useLocalStorageReducer from "../hooks/useLocalStorageReducer";
 
 const defaultTodos = [
-    { id: 1, task: "Mow the lawn", completed: false },
-    { id: 2, task: "Take out the trash", completed: true }
+  { id: 1, task: "Mow the lawn", completed: false },
+  { id: 2, task: "Take out the trash", completed: true },
 ];
 
 export const TodosContext = createContext();
 export const DispatchContext = createContext();
 
-export function TodosProvider({children}) {
-    const [todos, dispatch] = useReducer(todoReducer, defaultTodos);
+export function TodosProvider({ children }) {
+  const [todos, dispatch] = useLocalStorageReducer(
+    "todos",
+    defaultTodos,
+    todoReducer
+  );
 
-    return (
-        <TodosContext.Provider value={todos}>
-            <DispatchContext.Provider value={dispatch}>
-                {children}
-            </DispatchContext.Provider>
-        </TodosContext.Provider>
-    )
+  return (
+    <TodosContext.Provider value={todos}>
+      <DispatchContext.Provider value={dispatch}>
+        {children}
+      </DispatchContext.Provider>
+    </TodosContext.Provider>
+  );
 }
